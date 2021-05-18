e5c3e6d12ec4c95d27fd8ae6dc48e3e4a38405be
ambmax00 <maximilien.ambroise@gmail.com>
2021-05-18 17:00:29 +0200

    Fix targets for when clang format not found

--- cmake/modules/cpp_sources.cmake
+++ cmake/modules/cpp_sources.cmake
@@ -18,38 +18,43 @@ endif()
 
 function (ADD_CPP_SOURCES OUTVAR)
 
-	if (NOT CLANG_FORMAT_EXE) 
-		return()
+	if (CLANG_FORMAT_EXE) 
+
+	        set(outfiles)
+
+	        foreach (file ${ARGN})
+
+        	        get_filename_component(file "${file}" ABSOLUTE)
+               		get_filename_component(abs_path "${file}" DIRECTORY)
+
+                	file(RELATIVE_PATH rel_path "${CMAKE_SOURCE_DIR}" "${abs_path}")
+			set(CPP_GENERATED_SUBDIR "${CPP_GENERATED_DIR}/${rel_path}")
+			if (NOT EXISTS "${CPP_GENERATED_SUBDIR}")
+				file(MAKE_DIRECTORY "${CPP_GENERATED_SUBDIR}")
+                	endif()
+
+                	get_filename_component(root "${file}" NAME_WE)
+                	get_filename_component(extension "${file}" EXT)
+
+                	set(new_extension ".hpp")
+			set(of "${CPP_GENERATED_SUBDIR}/${root}${extension}")
+                	add_custom_command(
+                        	OUTPUT ${of}
+				COMMAND "${CMAKE_CXX_COMPILER}" -I ${CPP_INCLUDE_DIR} -E -DTEST_MACRO ${file} | ${CLANG_FORMAT_EXE} > ${of}
+                        	DEPENDS "${file}" "${CPP_DEFINITIONS}"
+                	)
+                	list(APPEND outfiles "${of}")
+
+        	endforeach()
+
+		add_custom_target("cpp_${OUTVAR}" ALL DEPENDS "${outfiles}")
+		add_dependencies(cpp_target "cpp_${OUTVAR}")
+
+	else()
+
+		add_custom_target("cpp_${OUTVAR}")
+                add_dependencies(cpp_target "cpp_${OUTVAR}")
+
 	endif()
-
-        set(outfiles)
-
-        foreach (file ${ARGN})
-
-                get_filename_component(file "${file}" ABSOLUTE)
-                get_filename_component(abs_path "${file}" DIRECTORY)
-
-                file(RELATIVE_PATH rel_path "${CMAKE_SOURCE_DIR}" "${abs_path}")
-		set(CPP_GENERATED_SUBDIR "${CPP_GENERATED_DIR}/${rel_path}")
-		if (NOT EXISTS "${CPP_GENERATED_SUBDIR}")
-			file(MAKE_DIRECTORY "${CPP_GENERATED_SUBDIR}")
-                endif()
-
-                get_filename_component(root "${file}" NAME_WE)
-                get_filename_component(extension "${file}" EXT)
-
-                set(new_extension ".hpp")
-		set(of "${CPP_GENERATED_SUBDIR}/${root}${extension}")
-                add_custom_command(
-                        OUTPUT ${of}
-			COMMAND "${CMAKE_CXX_COMPILER}" -I ${CPP_INCLUDE_DIR} -E -DTEST_MACRO ${file} | ${CLANG_FORMAT_EXE} > ${of}
-                        DEPENDS "${file}" "${CPP_DEFINITIONS}"
-                )
-                list(APPEND outfiles "${of}")
-
-        endforeach()
-
-	add_custom_target("cpp_${OUTVAR}" ALL DEPENDS "${outfiles}")
-	add_dependencies(cpp_target "cpp_${OUTVAR}")
 
 endfunction()
